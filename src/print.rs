c072ea0c00a245bdd92e35c7484071c4633e01e9
kamerk22 <kashyapk62@gmail.com>
2021-04-20 23:53:00 +0530

    print mod in main

--- src/print.rs
+++ src/print.rs
@@ -1,4 +1,29 @@
 pub fn run() {
     // Print to console
     println!("Hello from the print.rs");
+
+    // Basic Formatting
+    println!("{} is from {}", "Kashyap", "India");
+
+    //Positional Arguments
+    println!(
+        "{0} is from {1} and {0} likes to {2}",
+        "Kashyap", "India", "Code"
+    );
+
+    // Named Arguments
+    println!(
+        "{name} likes to play {activity}",
+        name = "John",
+        activity = "Cricket "
+    );
+
+    // Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octcal: {:o}", 10, 10, 10);
+
+    // Placeholder for debug traits
+    println!("{:?}", (12, true, "herllo"));
+
+    // Basic math
+    println!("10 + 10 = {}", 10 + 10);
 }
